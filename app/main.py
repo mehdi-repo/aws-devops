76726822c7549b696f3c11bd2136cca3f566b76d
mbosoftmedia <mbosoftmedia@gmail.com>
2025-02-08 20:32:50 +0100

    codeium

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Welcome to DepOps v.2"}
+    return {"message": "Hello Welcome to DepOps v.2 prefct work great"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
